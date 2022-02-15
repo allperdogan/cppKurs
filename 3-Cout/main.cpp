a792e6b8ae33098ff8f93698ed90fec450349b36
allperdogan <47662425+allperdogan@users.noreply.github.com>
2022-02-15 18:18:44 +0300

    Add files via upload

---
+++ 3-Cout/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int sayi;
+    cout << "Alper Dogan" << endl;
+    cout << "170504013" << endl;
+    printf("Stdio kutuphanesinde bunu kullanabiliriz C de ki gibi\n");
+    scanf("%d", &sayi);
+    printf("Sayi: %d",sayi);
+
+    return 0;
+}
