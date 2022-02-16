1ef33a99a8d5f095a60661be074896ec95826c4d
allperdogan <47662425+allperdogan@users.noreply.github.com>
2022-02-17 00:07:14 +0300

    Add files via upload

---
+++ 4-Degiskenler/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tamSayi;
+    float kesirliSayi;
+    double buyukKesirliSayi;
+    char karakter;
+    bool anahtar;
+
+    tamSayi = 5;
+    kesirliSayi = 7.2;
+    buyukKesirliSayi = 5.6;
+    karakter = 'K';
+    anahtar = true;
+
+    cout << tamSayi << " " << kesirliSayi <<
+    " " << buyukKesirliSayi << " "<< karakter<< " "<< anahtar <<endl;
+
+    return 0;
+}
