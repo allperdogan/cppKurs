4c325020c183e32f28f4bf90e6bab49334bdf645
allperdogan <47662425+allperdogan@users.noreply.github.com>
2022-02-18 23:53:54 +0300

    Add files via upload

---
+++ 6-Cin/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tamSayi;
+    cout << "Bir sayi giriniz!" << endl;
+    cin >> tamSayi;
+    cout << "Girilen sayi: " << tamSayi << endl;
+    return 0;
+}
