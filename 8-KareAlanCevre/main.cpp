667e6a42b3ea9c1cfaddeb873f5cfe6d1325abe6
allperdogan <47662425+allperdogan@users.noreply.github.com>
2022-02-19 20:40:18 +0300

    main.cpp

---
+++ 8-KareAlanCevre/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x;
+
+    cout<<"Karenin bir kenarini kenarini giriniz..."<<endl;
+    cin>>x;
+
+    cout<<"Karenin Cevresi: "<< 4*x <<endl<<"Karenin Alani: "<<x*x<<endl;
+
+    return 0;
+}
