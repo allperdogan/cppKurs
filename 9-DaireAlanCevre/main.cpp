781c7885873d52c55b312496fe1fe7577856f006
allperdogan <47662425+allperdogan@users.noreply.github.com>
2022-02-19 20:41:17 +0300

    Add files via upload

---
+++ 9-DaireAlanCevre/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float r;
+    cout << "Dairenin yaricapini giriniz..." << endl;
+    cin>>r;
+
+    cout << "Dairenin cevresi: " << 2*3.14*r << endl;
+    cout << "Dairenin alani " << 3.14*r*r << endl;
+
+    return 0;
+}
