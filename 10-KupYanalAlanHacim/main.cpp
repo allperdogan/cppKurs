7082eff4c6a140118737d37c56038d47a4a88e22
allperdogan <47662425+allperdogan@users.noreply.github.com>
2022-02-20 13:15:32 +0300

    main.cpp

---
+++ 10-KupYanalAlanHacim/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x;
+    cout << "Kupun bir kenarýný giriniz..." << endl;
+    cin>>x;
+
+    cout << "Yanal Alan Toplami: " << x*x*6 << endl;
+    cout << "Hacim: " << x*x*x << endl;
+
+    return 0;
+}
