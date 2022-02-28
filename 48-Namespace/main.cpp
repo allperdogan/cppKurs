5dd1ec721a8bbde796c049504b97410f57969637
allperdogan <47662425+allperdogan@users.noreply.github.com>
2022-02-28 23:11:51 +0300

    Add files via upload

---
+++ 48-Namespace/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//using namespace std;
+
+using std::cout;  //using namespace std kullanmak yerine böyle de yapılabilir.
+using std::endl;
+using std::cin;
+
+int main()
+{
+    int sayi;
+    cout << "Hello world!" << endl;
+    cin >> sayi;
+    return 0;
+}
