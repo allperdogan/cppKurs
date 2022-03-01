c08194ceb653cce5283a0dcb8427d85d89ccd1b6
Alper Doğan <47662425+allperdogan@users.noreply.github.com>
2022-03-01 23:48:38 +0300

    Add files via upload

---
+++ 60-NesneNedir/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// class -> sýnýf
+
+class Insan{
+    public:
+    string isim;
+    string gozRengi;
+};
+
+int main()
+{
+    Insan insan1;
+    Insan insan2;
+    insan1.isim = "Kaan";
+    insan1.gozRengi = "Ela";
+    insan2.isim = "Ali";
+    insan2.gozRengi = "Yesil";
+
+    cout << insan1.isim << " " << insan1.gozRengi << endl;
+    cout << insan2.isim << " " << insan2.gozRengi << endl;
+
+    return 0;
+}
