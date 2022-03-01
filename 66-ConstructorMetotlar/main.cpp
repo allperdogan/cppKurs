08885393881a8a3fc3752d4e97bcad4e95a68608
Alper Doğan <47662425+allperdogan@users.noreply.github.com>
2022-03-02 00:59:21 +0300

    Add files via upload

---
+++ 66-ConstructorMetotlar/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class Ogrenci{
+    private:
+    string isim;
+    int numara;
+
+    public:
+
+    Ogrenci(string yazilacakIsim, int yazilacakNumara){
+        isim = yazilacakIsim;
+        numara = yazilacakNumara;
+    }
+
+    void setIsim(string yazilacakIsim){
+        if(yazilacakIsim.size()>3){
+            isim = yazilacakIsim;
+        }
+        else{
+            cout << "Lutfen 3 harften fazla deger giriniz" << endl;
+        }
+    }
+    void setNumara(int yazilacakNumara){
+        if(yazilacakNumara>0) {
+            numara = yazilacakNumara;
+        }
+        else {
+            cout << "Lutfen 0 dan fazla deger giriniz" << endl;
+        }
+    }
+
+    void ogrenciBilgileriniGoster(){
+        cout << "Isim: " << isim << endl;
+        cout << "Numara: " << numara << endl;
+
+    }
+
+    string getIsim(){
+        return isim;
+    }
+    int getNumara(){
+        return numara;
+    }
+
+};
+
+int main()
+{
+    Ogrenci ogr1("Tugba",170501028);
+
+    ogr1.ogrenciBilgileriniGoster();
+
+    ogr1.setIsim("Alper");
+
+    ogr1.ogrenciBilgileriniGoster();
+
+    return 0;
+}
